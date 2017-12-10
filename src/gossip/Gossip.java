0b638b28adc650ec40bb1a17b6ce1253560a898a
Romil Jain <romil2707@gmail.com>
2017-12-10 17:57:55 -0500

    Added code for Getting Connected Node and Selecting Random Node

--- src/gossip/Gossip.java
+++ src/gossip/Gossip.java
@@ -10,6 +10,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 /**
  * @author romiljain
@@ -17,34 +18,57 @@ import java.util.List;
  */
 public class Gossip {
 	
-	public static void main (String args[]){
-		Gossip gossip = new Gossip();
-		List<String> nodeList = gossip.getConnectedNodes();
-		
-		System.out.println(nodeList);
+	List<String> nodes;
+	
+	private Random random;
+	
+	public Gossip(){
+		this.nodes = getConnectedNodes();
+		random = new Random();
 	}
 	
 	
-	public ArrayList<String> getConnectedNodes() {
-		ArrayList<String> nodesList = new ArrayList<String>();
+	public static void main (String args[]){
+		Gossip gossip = new Gossip();
+		gossip.pushMessage("Hello World");
+	}
+	
+	
+	private List<String> getConnectedNodes() {
+		nodes = new ArrayList<String>();
 		File nodeConfigFile = new File("config","nodes");
 
 		try {
 			BufferedReader br = new BufferedReader(new FileReader(nodeConfigFile));
 			String line;
 			while((line = br.readLine()) != null) {
-				nodesList.add(line.trim());
+				nodes.add(line.trim());
 			}
+			br.close();
 		} catch (FileNotFoundException e) {			
 			e.printStackTrace();
 		} catch (IOException e) {	
 			e.printStackTrace();
 		}
-		return nodesList;
+		return nodes;
 	}
 	
-	public void pushMessage() {
-		
+	public void pushMessage(String message) {
+		int length = nodes.size();
+		while(length>0){
+			String node = getRandomPeerNode();
+			System.out.println(node);
+			length--;
+		}
+	}
+	
+	private String getRandomPeerNode() {
+		String node = null;
+		if(nodes.size() > 0) {
+				int randomIndex = random.nextInt(nodes.size());
+				node = nodes.get(randomIndex);
+		} 
+		return node;
 	}
 	
 	public void pullMessage() {
