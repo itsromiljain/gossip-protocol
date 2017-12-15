71b74791337456766d220477412a86cc48ca66f5
Romil Jain <romil2707@gmail.com>
2017-12-14 23:22:17 -0500

    Updated code with Data Transfer (UDP)

--- src/gossip/Gossip.java
+++ src/gossip/Gossip.java
@@ -1,4 +1,4 @@
-/**
+ /**
  * 
  */
 package gossip;
@@ -8,7 +8,11 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
 
@@ -31,6 +35,13 @@ public class Gossip {
 		random = new Random();
 		infectedNodes = new ArrayList<String>();
 		removedNodes = new ArrayList<String>();
+		Iterator<String> itr = this.susceptibleNodes.iterator();
+		while(itr.hasNext()){
+			String node = itr.next();
+			String recipientNodePort = node.split(":")[1];
+			Thread listenermode = new Thread(new Listener(recipientNodePort));
+			listenermode.start();
+		}
 	}
 	
 	
@@ -43,7 +54,6 @@ public class Gossip {
 	private List<String> getConnectedNodes() {
 		susceptibleNodes = new ArrayList<String>();
 		File nodeConfigFile = new File("config","nodes");
-
 		try {
 			BufferedReader br = new BufferedReader(new FileReader(nodeConfigFile));
 			String line;
@@ -63,23 +73,17 @@ public class Gossip {
 		int length = susceptibleNodes.size();
 		while(length>0){
 			String node = getRandomPeerNode();
-			System.out.println(node);
-			// Update the node with the message
-			// Transmit the message
+			System.out.println("Random Node Selected: "+node);
+			String recipientNodeIp =node.split(":")[0];
+			int recipientNodePort = Integer.parseInt(node.split(":")[1]);
+			// Send message and update the node 
+			sendMessage (recipientNodeIp, recipientNodePort, message);
 			// Move the selected node to Infected node.
 			infectedNodes.add(node);
 			length--;
 		}
 	}
 	
-	private String getRandomPeerNode() {
-		String node = null;
-		if(susceptibleNodes.size() > 0) {
-				int randomIndex = random.nextInt(susceptibleNodes.size());
-				node = susceptibleNodes.get(randomIndex);
-		} 
-		return node;
-	}
 	
 	public void pullMessage() {
 		int length = susceptibleNodes.size();
@@ -91,5 +95,87 @@ public class Gossip {
 			}
 		}
 	}
+	
+	public void sendMessage(String recipientNodeIp, int recipientNodePort, String msgToSend) {
+		DatagramSocket socket;
+		byte[] buf = new byte[1024];
+		InetAddress address;
+		try {
+			//Initiate Socket
+			socket = new DatagramSocket();
+			address = InetAddress.getByName(recipientNodeIp);
+			buf = msgToSend.getBytes();
+			//Create packet to send to Recipient
+			DatagramPacket packet = new DatagramPacket(buf, buf.length, address, recipientNodePort);
+			socket.send(packet);
+			//New packet for receiving acknowledgement. 
+			packet = new DatagramPacket(buf, buf.length);
+			socket.receive(packet);
+			String received = new String(packet.getData(), 0, packet.getLength());
+			System.out.println("#DEBUG# - Acknowledgement - " + received);
+			//close the socket
+			socket.close();
+			//return received;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private String getRandomPeerNode() {
+		String node = null;
+		if(susceptibleNodes.size() > 0) {
+				int randomIndex = random.nextInt(susceptibleNodes.size());
+				node = susceptibleNodes.get(randomIndex);
+		} 
+		return node;
+	}
 
+}
+
+class MessageListener implements Runnable{
+	private DatagramSocket socket;
+	private boolean running;
+	private byte[] buf = new byte[1024];
+	private String port;
+
+	public MessageListener(String port) {
+		this.port= port;
+	}
+
+	@Override
+	public void run() {
+		running = true;
+		try {
+			
+			socket = new DatagramSocket(Integer.parseInt(port));
+			//Wait for receiving message
+			while (running) {
+				System.out.println("#INFO# - Listener Running waiting for message on IP - "+
+						InetAddress.getLocalHost().getHostAddress() + ", Port -"+port);
+				//New packet for receiving message. 
+				DatagramPacket packet = new DatagramPacket(buf, buf.length);
+				//receive packet from socket
+				socket.receive(packet);
+				InetAddress address = packet.getAddress();
+				int port = packet.getPort();
+				//Get message from packet
+				String received 
+				= new String(packet.getData(), 0, packet.getLength());
+				System.out.println("#DEBUG# - Server Received Message - '" + received+ "', From - "+address+", Port - "+port);
+				if (received.equals("end")) {
+					running = false;
+					continue;
+				}
+				buf = "ack-back".getBytes();
+				DatagramPacket returnPacket = new DatagramPacket(buf, buf.length, address, port);
+				socket.send(returnPacket);
+			}
+		socket.close();
+		System.out.println("#INFO# - Listener TERMINATED on IP - "+
+						InetAddress.getLocalHost().getHostAddress() + ", Port -"+port);
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
